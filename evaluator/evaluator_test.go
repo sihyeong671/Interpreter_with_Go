5e64f6310bf6bfcf9afc42a7adb7aa1c1e989735
sihyeong671 <bshlab671@naver.com>
2025-03-26 11:56:29 +0900

    add evaluator

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,67 @@
+package evaluator
+
+import (
+	"monkey-project/ast"
+	"monkey-project/lexer"
+	"monkey-project/object"
+	"monkey-project/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	// 테스트 인프라 구축 (확장이 쉽도록 설계)
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
